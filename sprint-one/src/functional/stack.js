8f7cebe445057fae9de83cc1f919708a2586d599
dmcin003 <dmcin003@fiu.edu>
2021-09-22 19:13:12 -0400

    Completed functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,25 +2,25 @@ var Stack = function() {
   var someInstance = {'top': 0}; // {top: 1, top: 2, top: 3 }
 
   // Use an object with numeric keys to store values
-  var storage5 = {}; // {1:string1,2:string2,3:string3}
+  var storage = {}; // {1:string1,2:string2,3:string3}
 
   // Implement the methods below
   someInstance.push = function(value) {
-    someInstance.top++;
-    storage5[top] = value;
+    someInstance['top']++;
+    storage[someInstance['top']] = value;
   };
 
   someInstance.pop = function() {
-    if (someInstance.top > 0) {
-      var temp = storage5[top];
-      delete storage5.top;
-      someInstance.top--;
+    if (someInstance['top'] > 0) {
+      var temp = storage[someInstance['top']];
+      delete storage[someInstance['top']];
+      someInstance['top']--;
       return temp;
     }
   };
 
   someInstance.size = function() {
-    return someInstance.top;
+    return someInstance['top'];
   };
 
   return someInstance;
