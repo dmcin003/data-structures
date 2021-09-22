6f44df467f7857f46ee809d6b88c93b20032e1ce
dmcin003 <dmcin003@fiu.edu>
2021-09-22 18:28:03 -0400

    Implemented push and size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
-  var someInstance = {top: 0};
+  var someInstance = {'top': 0}; // {top: 1, top: 2, top: 3 }
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage5 = {}; // {1:string1,2:string2,3:string3}
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.top++;
+    storage5[top] = value;
   };
 
   someInstance.pop = function() {
+    if (someInstance.top > 0) {
+      var temp = storage5[top];
+      delete storage5.top;
+      someInstance.top--;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return someInstance.top;
   };
 
   return someInstance;
