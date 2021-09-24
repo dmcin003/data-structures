988555e9a7225922b0bef5a58fccf518565c6dcb
dmcin003 <dmcin003@fiu.edu>
2021-09-24 00:09:08 -0400

    Completed set and linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,71 @@
 var LinkedList = function() {
-  var list = {};
+  var list = {}; // {head: null, tail: null,}
   list.head = null;
   list.tail = null;
 
+
+  // node1: {value: 4, next: null}, node2: {value: 5, next: null}}
+
   list.addToTail = function(value) {
+
+    /*
+
+
+    {
+      head: null,
+      tail: null
+    }
+
+
+    1. let newNode = node(2);
+    newNode.value = 2
+    newNode.next = newNode -> node(3)
+
+    head -> newNode
+    tail -> newNode
+
+    2. node(3) = NEW TAIL
+
+    newNode.value = 3
+    newNode.next = null -> node(4)
+
+    ..current tail? node(2) - > newNode -> ref1
+    current.tail.next -> newNode -> ref2
+
+
+    */
+    var newNode = Node(value);
+
+    // {head: newNode[0], tail: newNode[1]}   newNode[0] = {value: 4, next: newNode[1]};  newNode[1] = {value: 5, next: newNode[2]}
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var temp = list.head.next;
+    list.head.next = null;
+    var lastHeadValue = list.head.value;
+    list.head = temp;
+    return lastHeadValue;
   };
 
   list.contains = function(target) {
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    }
+
+    while (list.tail.next !== null) {
+      //do some work
+      if (list.tail.value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -17,7 +73,7 @@ var LinkedList = function() {
 
 var Node = function(value) {
   var node = {};
-
+  //{value:value,next:null}
   node.value = value;
   node.next = null;
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //O(1)
+  if (this._storage[item] === undefined) {
+    this._storage[item] = item;
+  }
 };
-
+// O(n)
 setPrototype.contains = function(item) {
+  var isTrue = false;
+  _.each(this._storage, function(value) {
+    if (value === item) {
+      isTrue = true;
+    }
+  });
+  return isTrue;
 };
 
 setPrototype.remove = function(item) {
+  //O(1)
+  if (this._storage[item] === item) {
+    delete this._storage[item];
+  }
 };
 
 /*
