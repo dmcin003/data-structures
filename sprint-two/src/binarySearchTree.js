48d1556172e93e61d0e24063939de05638652936
dmcin003 <dmcin003@fiu.edu>
2021-09-24 20:11:16 -0400

    Completed tree and binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,92 @@
 var BinarySearchTree = function(value) {
+  // A binary search tree can only have at most two children
+  var newBinarySearchTree = {};
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+  newBinarySearchTree.value = value;
+
+  _.extend(newBinarySearchTree, binarySearchTreeMethods);
+
+  return newBinarySearchTree;
 };
+
+// Methods
+var binarySearchTreeMethods = {
+
+  insert: function(value) {
+    var newTree = BinarySearchTree(value);
+    if (newTree.value > this.value) {
+
+      if (this.right === null) {
+        this.right = newTree;
+      } else {
+        //recurse
+        this.right.insert(value);
+      }
+    } else {
+      if (this.left === null) {
+        this.left = newTree;
+      } else {
+        // recurse
+        this.left.insert(value);
+      }
+    }
+  },
+
+  // contains
+  contains: function(target) {
+    // Check this trees value is the target
+    // if it is then return true
+    // is our target value greater then or less then this trees value
+    // Check if this trees left and right properties has children
+    // if they do we recurse through them
+    // return false
+    if (target === this.value) {
+      return true;
+    }
+
+    if (target > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        //recurse
+        if (this.right.contains(target)) {
+          return true;
+        }
+      }
+    } else {
+      if (this.left === null) {
+        return false;
+      } else {
+        if (this.left.contains(target)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
+  // [5, 2, 3]
+  // array.push;
+  // depthFirstLog
+  depthFirstLog: function(callback) {
+    // run the callback on the root tree value
+    callback(this.value);
+    // check if left is null
+    // if not, run recursion
+    if (this.left !== null) {
+      this.left.depthFirstLog(callback);
+    }
+    // check if right is null
+    // if not, run recursion
+    if (this.right !== null) {
+      this.right.depthFirstLog(callback);
+    }
+  }
+};
+
+
+
+
 
 
 /*
